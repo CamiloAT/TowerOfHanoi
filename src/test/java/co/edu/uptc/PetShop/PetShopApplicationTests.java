4788cc4230ff7e4f7855cb96e247af1eb87140dd
Camilo Arias <camilo.arias@uptc.edu.co>
2023-10-11 17:07:22 -0500

    Se sube la base

---
+++ src/test/java/co/edu/uptc/PetShop/PetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.PetShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
